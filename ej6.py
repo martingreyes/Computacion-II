04a6dbd42cee19f95d4f474a7390c71f4dca7cb6
Martin Reyes <martinreyes@MacBookPro.local>
2022-04-15 16:55:55 -0300

    Solamente hace la primer linea. Falta hacer los n hijos

--- ej6.py
+++ ej6.py
@@ -1,23 +1,3 @@
-# Escriba un programa que abra un archvo de texto pasado por argumento utilizando el modificador -f.
-# El programa deberá generar tantos procesos hijos como líneas tenga el archivo de texto.
-# El programa deberá enviarle, vía pipes (os.pipe()), cada línea del archivo a un hijo.
-# Cada hijo deberá invertir el orden de las letras de la línea recibida, y se lo enviará al proceso padre nuevamente, también usando os.pipe().
-# El proceso padre deberá esperar a que terminen todos los hijos, y mostrará por pantalla las líneas invertidas que recibió por pipe.
-
-# Ejemplo:
-# Contenido del archivo /tmp/texto.txt
-# Hola Mundo
-# que tal
-# este es un archivo
-# de ejemplo.
-
-# Ejecución:
-# python3 inversor.py -f /tmp/texto.txt
-# ovihcra nu se etse
-# .olpmeje ed
-# lat euq
-# odnuM aloH
-
 import argparse
 import subprocess
 import os
@@ -26,30 +6,80 @@ import time
 
 def main():
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-f", type=str, help= "path del archivo de texto", required=True)
-    args = parser.parse_args()
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-f", type=str, help= "path del archivo de texto", required=True)
+        args = parser.parse_args()
 
-    archivo = open(args.f, "r")
+        archivo = open(args.f, "r")
 
-    numero = sum(1 for line in archivo)
+        lineas = archivo.readlines()
 
-    padre = os.getpid()
+        numero = sum(1 for line in archivo)
 
-    for x in range(numero):
-      
+        padre = os.getpid()
+
+        r, w = os.pipe()
+
+        r2, w2 = os.pipe()
+
+        contador = 0
+        
         os.fork()
 
-        if os.getpid() != padre:      # Lo que esta dentro de este if solamente lo hacen los hijos
+        if os.getpid() == padre:
 
+            # primero el padre escribe
 
-            os._exit(0) 
+            os.close(r)
 
-    
-    for i in range(args.n):
-        os.wait()                     # padre espera a hijos (?) 
+            w = os.fdopen(w, 'w')
 
+            print("Padre escribiendo: {}".format(lineas[contador]))
+
+            w.write(lineas[contador])
+
+            w.close()
+
+            # ahora empieza a leer el padre
+
+            os.close(w2)
+
+            r2 = os.fdopen(r2)
+
+            string2 = r2.read()
+
+            print("\nPadre leyendo: \n{}".format(string2))
+
+            os._exit(0)
+
+            contador = contador + 1
+
+        if os.getpid() != padre:        # Lo que esta dentro de este if solamente lo hacen los hijos
+
+            # primero el hijo lee
+
+            os.close(w)
+            
+            r = os.fdopen(r)
+
+            string = r.read()
+
+            print("Hijo {} leyendo: {}".format(os.getpid(), string))
+
+            # ahora empieza a escribir el hijo
+
+            os.close(r2)
+
+            w2 = os.fdopen(w2, 'w')
+
+            print("\nHijo {} escribiendo: \n{}".format(os.getpid(), string[::-1]))
+
+            w2.write(string[::-1])
+
+            w2.close()
+
+            os._exit(0)            
+
+        
 if __name__=="__main__":
     main()
-
-# Correr con p ej6.py -f /Users/martinreyes/Downloads/texto.txt
