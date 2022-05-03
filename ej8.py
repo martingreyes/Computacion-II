85e609ea831f34f95b798a8fc739d9254880b91b
Martin Reyes <martinreyes@MacBookPro.local>
2022-05-03 16:16:27 -0300

    Solamente falta agregar condicion para que termine el programa

--- ej8.py
+++ ej8.py
@@ -9,25 +9,26 @@ El primer hijo deberá leer desde dicha cola de mensajes y mostrar el contenido 
 
 import multiprocessing, sys, os, string, time
 
-def primer_hijo(pipe_w, cola): 
-    sys.stdin = open(0)                                                                  
-    print("Ingrese una línea: ")
-    linea = sys.stdin.readline().rstrip("\n")                                           # 1) Hijo 1 lee (tambien le quito el salto de linea al readline)
-    pipe_w.send(linea.lower())                                                          # 2) Hijo 1 escribe en pipe 
-    
-    print(cola.get())                                                                   # 5) Hijo 1 lee contenido de la cola y lo muestra por pantalla
-    
+def primer_hijo(pipe_w, cola):
+    sys.stdin = open(0)  
+    while True:                                                              
+        print("Ingrese una línea: ") 
+        linea = sys.stdin.readline().rstrip("\n")                                           # 1) Hijo 1 lee (tambien le quito el salto de linea al readline)
+        pipe_w.send(linea.lower())                                                          # 2) Hijo 1 escribe en pipe 
+        
+        print(cola.get())                                                                   # 5) Hijo 1 lee contenido de la cola y lo muestra por pantalla
+        
     # print("Hijo 1 ({}) muriendo".format(os.getpid()))
 
 def segundo_hijo(cola, pipe_r):
-    linea = pipe_r.recv()                                                               # 3) Hijo 2 lee el pipe
-    encriptado = ""
-    for letra in linea:
-                                                  
-            letra = string.ascii_letters[string.ascii_letters.index(letra) + 13 ].upper()
-            encriptado = encriptado + letra 
-                                          
-    cola.put(encriptado)                                                                # 4) Hijo 2 almacena en cola de mensajes el texto leido encriptado
+    while True:
+        linea = pipe_r.recv()                                                               # 3) Hijo 2 lee el pipe
+        encriptado = ""
+        for letra in linea:                               
+                letra = string.ascii_letters[string.ascii_letters.index(letra) + 13 ].upper()
+                encriptado = encriptado + letra 
+                                            
+        cola.put(encriptado)                                                                # 4) Hijo 2 almacena en cola de mensajes el texto leido encriptado
     
     # print("Hijo 2 ({}) muriendo".format(os.getpid()))
     
