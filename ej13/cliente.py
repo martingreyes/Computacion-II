3a04c01b1419c8c6983f85fe2847a4359dff7418
Martin Reyes <martinreyes@macOS.local>
2022-09-01 16:31:20 -0300

    Last commit

---
+++ ej13/cliente.py
@@ -0,0 +1,28 @@
+import socket, sys, argparse, pickle
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", help= "dirección IP o nombre del servidor al que conectarse")
+parser.add_argument("-p", type=int, help= "puerto donde va atender el servidor")
+args = parser.parse_args()
+
+host = args.d
+puerto = args.p
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host,puerto))
+
+while(True) :
+    command = input('+ Enter a command to send : ')
+    command = pickle.dumps(command)
+    s.send(command)
+    
+    # receive data from client (data, addr)
+    msg = s.recv(1024)
+    msg = pickle.loads(msg)
+
+    print('- Server reply : '+ msg)
+
+    if msg == "Chau chau":
+        break
+
+#? Correr con p cliente.py -d 127.0.0.1 -p 1234
---
+++ ej13/server.py
@@ -0,0 +1,100 @@
+# Escriba un programa cliente/servidor en python que permita ejecutar comandos GNU/Linux en una computadora remota.
+# Técnicamente, se deberá ejecutar un código servidor en un equipo “administrado”, y programar un cliente (administrador) 
+# que permita conectarse al servidor mediante sockets STREAM.
+
+# El cliente deberá darle al usuario un prompt en el que pueda ejecutar comandos de la shell.
+# Esos comandos serán enviados al servidor, el servidor los ejecutará, y retornará al cliente:
+#     la salida estándar resultante de la ejecución del comando
+#     la salida de error resultante de la ejecución del comando.
+#     El cliente mostrará en su consola local el resultado de ejecución del comando remoto, ya sea que se haya realizado correctamente o no, anteponiendo un OK o un ERROR según corresponda.
+
+# El servidor debe atender solicitudes utilizando sockets de alto nivel con serversocket.
+# El servidor debe poder recibir las siguientes opciones:
+#     -p <port>: puerto donde va a atender el servidor.
+#     -c p | t : modo de concurrencia. Si la opción es "-c p" el servidor generará un nuevo proceso al recibir conexiones nuevas. Si la opción es "-c t" generará hilos nuevos.
+
+# El cliente debe poder recibir las siguientes opciones:
+#     -d <host> : dirección IP o nombre del servidor al que conectarse.
+#     -p <port> : número de puerto del servidor.
+# Para leer estos argumentos se recomienda usar módulos como argparse o click.
+
+# Ejemplo de ejecución del cliente (la salida de los comandos corresponden a la ejecución en el equipo remoto.
+
+# diego@cryptos$ python3 ejecutor_cliente.py -h 127.0.0.1 -p 2222
+# > pwd
+# OK
+# /home/diego
+# > ls -l /home
+# OK
+# drwxr-xr-x 158 diego diego 20480 May 26 18:57 diego
+# drwx------   2 root  root  16384 May 28  2014 lost+found
+# drwxr-xr-x   6 andy  andy   4096 Jun  4  2015 user
+# > ls /cualquiera
+# ERROR
+# ls: cannot access '/cualquiera': No such file or directory
+# >
+
+import argparse, socketserver, pickle, subprocess
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+
+        while True:
+
+            command = self.request.recv(1024)
+            command = pickle.loads(command)
+            print("- {} {} : {}".format(self.client_address[0], self.client_address[1], command))
+
+            if command == "exit":
+                dato = pickle.dumps("Chau chau")
+                self.request.sendall(dato) 
+                break   
+                s.close()
+                sys.exit(0)   
+                        
+
+            p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,  universal_newlines=True, bufsize=10000)
+            salida, error = p.communicate()
+            
+            if salida == "":
+                dato = pickle.dumps(error)
+                self.request.sendall(dato)
+            
+            if error == "":
+                dato = pickle.dumps(salida)
+                self.request.sendall(dato)
+        
+class ForkedTCPServer(socketserver.ForkingMixIn, socketserver.TCPServer):
+    pass
+
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", type=int, help= "puerto donde va atender el servidor")
+    parser.add_argument("-c",type=str, help= "modo de concurrencia")
+    args = parser.parse_args()
+
+    puerto = args.p
+    concurrencia = args.c
+    address = ("localhost", puerto)
+
+    if concurrencia.lower() == "p":
+        socketserver.TCPServer.allow_reuse_address = True
+        with ForkedTCPServer(address, MyTCPHandler) as server:
+            server.serve_forever()
+ 
+
+    if concurrencia.lower() == "t":
+        socketserver.TCPServer.allow_reuse_address = True
+        with ThreadedTCPServer((HOST, PORT), MyTCPHandler) as server:
+            server.serve_forever()
+           
+   
+
+#? Correr con p server.py -p 1234 -c p
+
+
+
