8f4a1995994b334093c34237e950c9eda9687ce4
Martin Reyes <martinreyes@macOS.lan>
2023-04-18 16:33:48 -0300

    BD

--- final/app/trivia.py
+++ final/app/trivia.py
@@ -1,6 +1,6 @@
-import sqlite3, os
+import sqlite3
 
-conexion = sqlite3.connect("{}/app/trivia.db".format(os.getcwd()))
+conexion = sqlite3.connect("/trivia.db".format())
 
 preguntas = [(0, "¿Capital de Brasil?"),
             (1, "¿Nacionalidad de Roger Federer?"),
